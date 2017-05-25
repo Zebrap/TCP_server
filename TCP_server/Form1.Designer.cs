0c7e17ff7079c9a2eaef4dcb0c73bc7dca8f6de5
Wojciech <wojciech.dywan@interia.pl>
2017-05-25 17:26:59 +0200

    Stworzono interfejs

--- TCP_server/Form1.Designer.cs
+++ TCP_server/Form1.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.adres = new System.Windows.Forms.TextBox();
+            this.my_port = new System.Windows.Forms.NumericUpDown();
+            this.info_o_polaczeniu = new System.Windows.Forms.ListBox();
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.my_port)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // adres
+            // 
+            this.adres.Location = new System.Drawing.Point(24, 12);
+            this.adres.Name = "adres";
+            this.adres.Size = new System.Drawing.Size(289, 20);
+            this.adres.TabIndex = 0;
+            this.adres.Text = "adres";
+            // 
+            // my_port
+            // 
+            this.my_port.Location = new System.Drawing.Point(370, 12);
+            this.my_port.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.my_port.Name = "my_port";
+            this.my_port.Size = new System.Drawing.Size(289, 20);
+            this.my_port.TabIndex = 1;
+            // 
+            // info_o_polaczeniu
+            // 
+            this.info_o_polaczeniu.FormattingEnabled = true;
+            this.info_o_polaczeniu.Location = new System.Drawing.Point(24, 38);
+            this.info_o_polaczeniu.Name = "info_o_polaczeniu";
+            this.info_o_polaczeniu.Size = new System.Drawing.Size(635, 264);
+            this.info_o_polaczeniu.TabIndex = 2;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(160, 321);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(153, 29);
+            this.start.TabIndex = 3;
+            this.start.Text = "start";
+            this.start.UseVisualStyleBackColor = true;
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(370, 321);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(169, 30);
+            this.stop.TabIndex = 4;
+            this.stop.Text = "stop";
+            this.stop.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 363);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.info_o_polaczeniu);
+            this.Controls.Add(this.my_port);
+            this.Controls.Add(this.adres);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.my_port)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox adres;
+        private System.Windows.Forms.NumericUpDown my_port;
+        private System.Windows.Forms.ListBox info_o_polaczeniu;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
     }
 }
 
